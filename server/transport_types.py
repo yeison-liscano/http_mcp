428da498a7dd751ae97b861f301ec687e7a3b79e
Yeison Liscano <yliscanoc@unal.edu.co>
2025-08-16 18:41:15 -0500

    feat(package): #5 increase tests coverage
    - add tests
    - remove unreachable code
    - increase coverage threshold

---
+++ server/transport_types.py
@@ -0,0 +1,19 @@
+from enum import IntEnum
+from http import HTTPStatus
+
+from pydantic import BaseModel
+
+
+class ProtocolErrorCode(IntEnum):
+    INVALID_PARAMS = -32602
+    INTERNAL_ERROR = -32603
+    METHOD_NOT_FOUND = -32601
+    RESOURCE_NOT_FOUND = -32002
+
+
+class ErrorResponseInfo(BaseModel):
+    message_id: int | str
+    protocol_code: ProtocolErrorCode
+    http_status_code: HTTPStatus
+    message: str
+    headers: dict[str, str] | None = None
---
+++ tests/test_base_transport.py
@@ -0,0 +1,123 @@
+from http import HTTPStatus
+
+from starlette.testclient import TestClient
+
+from server.transport_types import ProtocolErrorCode
+from tests.models import DUMMY_SERVER
+
+
+def test_initialize_bad_request() -> None:
+    client = TestClient(DUMMY_SERVER.app)
+
+    response = client.post(
+        "/mcp",
+        json={
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "initialize",
+            "params": {
+                "protocolVersion": "2025-06-18",
+            },
+        },
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "error": {
+            "code": ProtocolErrorCode.INVALID_PARAMS.value,
+            "message": '[{"type": "missing", "loc": ["params", "clientInfo"], '
+            '"msg": "Field required", "input": {"protocolVersion": '
+            '"2025-06-18"}, "url": '
+            '"https://errors.pydantic.dev/2.10/v/missing"}, {"type": '
+            '"missing", "loc": ["params", "capabilities"], "msg": '
+            '"Field required", "input": {"protocolVersion": '
+            '"2025-06-18"}, "url": '
+            '"https://errors.pydantic.dev/2.10/v/missing"}]',
+        },
+    }
+
+
+def test_initialize_unsupported_version() -> None:
+    client = TestClient(DUMMY_SERVER.app)
+
+    response = client.post(
+        "/mcp",
+        json={
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "initialize",
+            "params": {
+                "protocolVersion": "2024-11-05",
+                "capabilities": {
+                    "roots": {
+                        "listChanged": True,
+                    },
+                    "sampling": {},
+                    "elicitation": {},
+                },
+                "clientInfo": {
+                    "name": "ExampleClient",
+                    "title": "Example Client Display Name",
+                    "version": "1.0.0",
+                },
+            },
+        },
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "error": {
+            "code": ProtocolErrorCode.INVALID_PARAMS.value,
+            "message": "Unsupported protocol version",
+            "data": {"supported": ["2025-06-18"], "requested": "2024-11-05"},
+        },
+    }
+
+
+def test_method_not_found() -> None:
+    client = TestClient(DUMMY_SERVER.app)
+
+    response = client.post(
+        "/mcp",
+        json={"jsonrpc": "2.0", "method": "invalid", "id": 1},
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "error": {
+            "code": ProtocolErrorCode.METHOD_NOT_FOUND.value,
+            "message": "Error validating message request",
+        },
+    }
+
+
+def test_invalid_tool_execution_request() -> None:
+    client = TestClient(DUMMY_SERVER.app)
+
+    response = client.post(
+        "/mcp",
+        json={
+            "jsonrpc": "2.0",
+            "method": "tools/call",
+            "id": 1,
+        },
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "error": {
+            "code": ProtocolErrorCode.INVALID_PARAMS.value,
+            "message": '[{"type": "model_type", "loc": ["params"], "msg": "Input should be a '
+            'valid dictionary or instance of ToolsCallRequestParams", "input": '
+            'null, "ctx": {"class_name": "ToolsCallRequestParams"}, "url": '
+            '"https://errors.pydantic.dev/2.10/v/model_type"}]',
+        },
+    }
